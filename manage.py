93188afba2c0e851735c4ad2c69f509bed77ff8b
summus2020 <83839962+summus2020@users.noreply.github.com>
2022-04-26 22:50:57 +0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ from flask_migrate import Migrate, MigrateCommand
 from app import app
 from models import db
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://xlhvqyldvmigns:93709ec366f14884dd2f666a007bf463a1f125c0696b3cd7f301230a85753a2a@ec2-18-214-195-34.compute-1.amazonaws.com:5432/d966buti312f0r" # "postgresql://postgres:postgres@localhost:5432/alex" #
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost:5432/alex" #
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 migrate = Migrate(app, db)
